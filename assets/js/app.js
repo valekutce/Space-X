389fd8540e925506d0289aaa17f972805fc1489f
валерий <valekutce@gmail.com>
2023-01-16 17:46:59 +0200

    доработал

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,23 @@
     headerWrap = document.querySelector('.header_wrap'),
     burg = document.querySelector('.burg'),
     clockMenu = document.querySelector('.clock-menu');
-    backdrop = document.querySelector('.backdrop');
+    backdrop = document.querySelector('.backdrop'),
+    mobileLinks = document.querySelectorAll('.header_nav ul.nav-items li.menu-item a');
+
+    window.addEventListener('load', ()=> {
+        setTimeout(()=> {
+            backdrop.classList.remove('backdrop_active'); 
+            backdrop.classList.remove('backdrop_start');
+        }, 6800);
+    });
+
+    mobileLinks.forEach(mobileLink => {
+        mobileLink.addEventListener('click', () => {
+            headerWrap.classList.remove('header_wrap_active');
+            backdrop.classList.remove('backdrop_active');
+            burg.style.display = 'block';
+        });
+    });
 
     window.addEventListener('scroll', function() {
         if (this.scrollY > 1) {
@@ -19,9 +35,19 @@
     burg.addEventListener('click', () => {
         headerWrap.classList.add('header_wrap_active');
         backdrop.classList.add('backdrop_active');
+        burg.style.display = 'none';
     });
 
     clockMenu.addEventListener('click', () => {
         headerWrap.classList.remove('header_wrap_active');
         backdrop.classList.remove('backdrop_active');
+        burg.style.display = 'block';
     });
+
+
+    // backdrop.addEventListener('click', () => {
+    //     backdrop.classList.remove('backdrop_active');
+    //     headerWrap.classList.remove('header_wrap_active');
+    //     burg.style.display = 'block';
+    // });
+    
